b3db1e13866dc6e8d551402225f0c99bb8942f64
Steven <stevengf14@gmail.com>
2024-06-24 13:21:05 -0500

    Solution

--- src/main/java/ec/com/linkedinlearning/objectorientedprogramming/C2/BankAccount.java
+++ src/main/java/ec/com/linkedinlearning/objectorientedprogramming/C2/BankAccount.java
@@ -5,7 +5,36 @@ package ec.com.linkedinlearning.objectorientedprogramming.C2;
  * @author steve
  */
 public class BankAccount {
-    
-    String owner;
-    double balance;
+
+    private String owner;
+    private double balance;
+
+    public BankAccount(String owner, double balance) {
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
 }
--- src/main/java/ec/com/linkedinlearning/objectorientedprogramming/C2/Main.java
+++ src/main/java/ec/com/linkedinlearning/objectorientedprogramming/C2/Main.java
@@ -7,7 +7,7 @@ package ec.com.linkedinlearning.objectorientedprogramming.C2;
 public class Main {
 
     public static void main(String[] args) {
-        Tree myFavoriteMapleTree = new Tree(90, 30, TreeType.MAPLE);
+        /*Tree myFavoriteMapleTree = new Tree(90, 30, TreeType.MAPLE);
 
         System.out.println(myFavoriteMapleTree.getTreeType());
         System.out.println(myFavoriteMapleTree.getHeightFt());
@@ -15,5 +15,15 @@ public class Main {
 
         // myFavoriteMapleTree.heightFt = 180;
         myFavoriteMapleTree.grow();
+        
+        String name = "Kathryn";
+        name.length();*/
+
+        // Challenge!
+        BankAccount ba = new BankAccount("Steven", 200);
+        ba.withdraw(100);
+
+        System.out.println("Balance: " + ba.getBalance());
+
     }
 }
