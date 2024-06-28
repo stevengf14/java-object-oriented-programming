02db8e09f96c287d1c695ecf36e94a9a57aab804
Steven <stevengf14@gmail.com>
2024-06-27 23:13:49 -0500

    Writing flexible code with compile time polymorphism

--- src/main/java/ec/com/linkedinlearning/objectorientedprogramming/C4/ConditionArrayList.java
+++ src/main/java/ec/com/linkedinlearning/objectorientedprogramming/C4/ConditionArrayList.java
@@ -3,6 +3,7 @@ package ec.com.linkedinlearning.objectorientedprogramming.C4;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
@@ -15,10 +16,14 @@ public class ConditionArrayList extends ArrayList<Integer> {
 
     private Predicate<Integer> condition;
 
-    public ConditionArrayList(Predicate<Integer> predicate, Integer... nums) {
-        super(Arrays.stream(nums)
-                .filter(predicate).collect(Collectors.toList()));
-        this.condition = condition;
+    public ConditionArrayList(Predicate<Integer> predicate,
+            Integer... nums) {
+        super(new ConditionArrayList(predicate, Arrays.asList(nums)));
+    }
+
+    public ConditionArrayList(Predicate<Integer> predicate, List<Integer> arrayList) {
+        super(arrayList.stream().filter(predicate).collect(Collectors.toList()));
+        this.condition = predicate;
     }
 
     @Override
@@ -38,28 +43,39 @@ public class ConditionArrayList extends ArrayList<Integer> {
     }
 
     @Override
-    public void replaceAll(UnaryOperator<Integer> uo) {
-        super.replaceAll(uo); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
     public boolean addAll(Collection<? extends Integer> c) {
-        return super.addAll(c); //To change body of generated methods, choose Tools | Templates.
+        return super.addAll(c.stream()
+                .filter(this::isEligible)
+                .collect(Collectors.toList()));
     }
 
     @Override
-    public Integer set(int index, Integer element) {
-        return super.set(index, element); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends Integer> c) {
+    public boolean addAll(int index,
+            Collection<? extends Integer> c) {
         return super.addAll(index, c.stream()
                 .filter(this::isEligible)
                 .collect(Collectors.toList()));
     }
 
+    @Override
+    public Integer set(int index,
+            Integer element) {
+        if (isEligible(element)) {
+            return super.set(index, element);
+        } else {
+            System.out.println(element + " is not odd. ");
+            return Integer.MIN_VALUE;
+        }
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<Integer> operator) {
+        super.replaceAll(operator);
+        super.removeIf(n -> !isEligible(n));
+    }
+
     public boolean isEligible(Integer element) {
         return condition.test(element);
     }
+
 }
--- src/main/java/ec/com/linkedinlearning/objectorientedprogramming/C4/Main.java
+++ src/main/java/ec/com/linkedinlearning/objectorientedprogramming/C4/Main.java
@@ -2,6 +2,7 @@ package ec.com.linkedinlearning.objectorientedprogramming.C4;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.function.Predicate;
 
 /**
  *
@@ -10,52 +11,28 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        /*ConditionArrayList oddListy = new ConditionArrayList(1, 2, 3, 4, 7, 9, -13);
 
-        System.out.println(oddListy);
+        Predicate<Integer> isDivisibleByThree = n -> Math.abs(n) % 3 == 0;
+        ConditionArrayList divisibleByThreeListy
+                = new ConditionArrayList(isDivisibleByThree);
 
-        ArrayList<Integer> listy = new ArrayList<>();
-        listy.add(1);
-        listy.add(2);
-        listy.add(3);
-        listy.add(4);
-        listy.add(7);
-        listy.add(9);
-        listy.add(-13);
+        ConditionArrayList divisibleByThreeListy2
+                = new ConditionArrayList(isDivisibleByThree,
+                        1, 2, 3, 4, 5, 6, 9);
 
-        System.out.println(listy);*/
+        ArrayList<Integer> numsList = new ArrayList<>();
+        numsList.add(1);
+        numsList.add(4);
+        numsList.add(3);
+        numsList.add(6);
+        numsList.add(13);
 
-        ConditionArrayList oddListy = new ConditionArrayList(n -> Math.abs(n) % 2 == 1);
-        oddListy.add(1);
-        oddListy.add(2);
-        addRandomNumber(oddListy);
-        System.out.println(oddListy);
+        ConditionArrayList divisibleByThreeListy3
+                = new ConditionArrayList(isDivisibleByThree,
+                        numsList);
 
-        ConditionArrayList conditionListy = new ConditionArrayList(n -> Math.abs(n) % 2 == 0);
-        conditionListy.add(1);
-        conditionListy.add(2);
-        addRandomNumber(conditionListy);
-        System.out.println(conditionListy);
-
-        ArrayList<Integer> listy = new ArrayList<>();
-        listy.add(1);
-        listy.add(2);
-        addRandomNumber(listy);
-        System.out.println(listy);
-
-    }
-
-    public static void addRandomNumber(ArrayList<Integer> list) {
-        Random random = new Random();
-        int n = random.nextInt(1000);
-
-        if (list instanceof ConditionArrayList) {
-            ConditionArrayList conditionList = (ConditionArrayList) list;
-            while (!conditionList.isEligible(n)) {
-                n = random.nextInt(1000);
-            }
-        }
-        list.add(n);
+        Predicate<Integer> isDivisibleBySix = n -> Math.abs(n) % 6 == 0;
+        ConditionArrayList divisibleBySixList = new ConditionArrayList(isDivisibleBySix, divisibleByThreeListy);
     }
 
 }
