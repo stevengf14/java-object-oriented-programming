702deaac44f5193ca8523c45f3a2c481b44cb42e
Steven <stevengf14@gmail.com>
2024-06-27 22:54:06 -0500

    Accessing specific object form in a single object

--- src/main/java/ec/com/linkedinlearning/objectorientedprogramming/C4/Main.java
+++ src/main/java/ec/com/linkedinlearning/objectorientedprogramming/C4/Main.java
@@ -1,6 +1,7 @@
 package ec.com.linkedinlearning.objectorientedprogramming.C4;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
@@ -9,7 +10,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        OddArrayList oddListy = new OddArrayList(1, 2, 3, 4, 7, 9, -13);
+        /*OddArrayList oddListy = new OddArrayList(1, 2, 3, 4, 7, 9, -13);
 
         System.out.println(oddListy);
 
@@ -22,7 +23,29 @@ public class Main {
         listy.add(9);
         listy.add(-13);
 
+        System.out.println(listy);*/
+
+        OddArrayList oddListy = new OddArrayList(7);
+        oddListy.add(1);
+        oddListy.add(2);
+        addRandomNumber(oddListy);
+        System.out.println(oddListy);
+
+        ArrayList<Integer> listy = new ArrayList<>();
+        listy.add(1);
+        listy.add(2);
+        addRandomNumber(listy);
         System.out.println(listy);
+
+    }
+
+    public static void addRandomNumber(ArrayList<Integer> list) {
+        int originalSize = list.size();
+        Random random = new Random();
+        while (originalSize + 1 != list.size()) {
+            int n = random.nextInt(1000);
+            list.add(n);
+        }
     }
 
 }
